d2a9d6e988ee97852f67aa1f3ceafb4a410c999f
Younes Essoufi <younes.essoufi@etu.unice.fr>
2024-01-23 12:26:40 +0100

    fonction save

--- app/models/User.php
+++ app/models/User.php
@@ -12,12 +12,17 @@ class User
     public $last_name;
     public $birthdate;
     public $phone;
+    public $Admin
 
     // Méthode pour enregistrer un nouvel utilisateur
     public function save()
     {
-        // TODO Save dans la BDD
+        if($_POST)
+            executeRequete("INSERT INTO USERS (Nom, Prenom, Email, MDP, Admin, tel, date_naissance) VALUES ('$_POST[last_name]', '$_POST[first_name]', '$_POST[Email]', '$_POST[password]', $_POST[Admin], $_POST[phone],$_POST[birthdate])");
+        else
+            echo "ca marche pas le sang";
     }
+    
 
     // Méthode pour récupérer un utilisateur par son ID
     public static function getById($id)
