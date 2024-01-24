7007d2312e9475f3826a7f88c4228e6c32e42ff2
Ilyane <ilyane.selmane@etu.unice.fr>
2024-01-24 15:52:38 +0100

    ptit changement du authcontroller
    grzgrereg

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
-    include("Controller.php");
-    include("../models/User.php");
+    require_once("Controller.php");
+    require_once("../models/User.php");
 class AuthController extends Controller
 {
 
@@ -25,7 +25,8 @@ class AuthController extends Controller
 
         // Exemple basique de validation (à adapter selon votre modèle)
         if ($user = User::getByEmail($email)) {
-            if (password_verify($password, $user->password)) {
+            if (password_verify($password, $user->password))
+            {
                 // Authentification réussie
                 $_SESSION['user_id'] = $user->id;
                 // Rediriger vers la page d'accueil ou une autre page après la connexion
@@ -48,18 +49,21 @@ class AuthController extends Controller
     // Méthode pour traiter la soumission du formulaire d'inscription
     public function register()
     {
-
         // Votre logique d'enregistrement d'utilisateur ici
         // Assurez-vous de valider les données entrées par l'utilisateur
 
-        $user = new User();
-        $user->email = $_POST['email'];
-        $user->password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hachage sécurisé du mot de passe
-        $user->first_name = $_POST['first_name'];
-        $user->last_name = $_POST['last_name'];
-        $user->birthdate = $_POST['birthdate'];
-        $user->phone = $_POST['phone'];
+        $userData = [
+            'IDuser' => null,  // Laissez IDuser comme null, car il sera généré automatiquement dans la base de données (suppose que c'est une clé primaire auto-incrémentée)
+            'Email' => $_POST['email'],
+            'MDP' => password_hash($_POST['password'], PASSWORD_DEFAULT),
+            'Prenom' => $_POST['first_name'],
+            'Nom' => $_POST['last_name'],
+            'date_naissance' => $_POST['birthdate'],
+            'tel' => $_POST['phone'],
+            'Admin' => null  // Laissez Admin comme null par défaut, ou ajustez selon vos besoins
+        ];
 
+        $user = new User($userData);
         $user->save(); // Enregistrement de l'utilisateur dans la base de données
 
         // Rediriger vers la page de connexion après l'inscription
